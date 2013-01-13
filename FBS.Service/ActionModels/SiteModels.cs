1606572c5e2b34976f2bffbf6f998ab2aa94e6e0
mike442144 <mike442144@localhost>
2013-01-13 06:51:37 +0000

    add service files

---
+++ FBS.Service/ActionModels/SiteModels.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+
+namespace FBS.Service.ActionModels
+{
+    [DisplayName("许可协议")]
+    public class License
+    {
+        [DisplayName("许可协议")]
+        public string Info { get; set; }
+    }
+
+    [DisplayName("运行环境检查")]
+    public class CheckEvn
+    {
+        public string OSVersion;
+        public string RunTimeVersion;
+    }
+
+    [DisplayName("配置数据库")]
+    public class DbCnf
+    {
+        [DisplayName("数据库名称")]
+        public string DbName { get; set; }
+        [DisplayName("数据库用户名")]
+        public string DbUser { get; set; }
+        [DisplayName("数据库密码")]
+        public string DbPsd { get; set; }
+        [DisplayName("数据库地址")]
+        public string DbAddr { get; set; }
+        //[DisplayName("数据库表前缀")]
+        //public string DbTablePrefix { get; set; }
+    }
+
+    [DisplayName("配置网站信息")]
+    public class SiteCnf
+    {
+        public SiteCnf()
+        {
+            this.SiteName = string.Empty;
+            this.SiteDesc = string.Empty;
+            this.SiteUrl = string.Empty;
+            this.CopyRight = string.Empty;
+            this.Version = new Version();
+            this.FounderEmail = string.Empty;
+            this.FounderName = string.Empty;
+            this.FounderPsd = string.Empty;
+        }
+
+        [DisplayName("网站名称")]
+        public string SiteName { get; set; }
+        [DisplayName("网站描述")]
+        public string SiteDesc { get; set; }
+        [DisplayName("您的网址")]
+        public string SiteUrl { get; set; }
+        [DisplayName("创始人用户名称")]
+        public string FounderName { get; set; }
+        [DisplayName("密码")]
+        public string FounderPsd { get; set; }
+        [DisplayName("联系邮箱")]
+        public string FounderEmail { get; set; }
+        public string CopyRight{get;set;}
+        public Version Version;
+    }
+    [DisplayName("网站设置")]
+    public class SiteSettings
+    {
+        [DisplayName("主题")]
+        public string ThemeName { get; set; }
+        [DisplayName("网站编号")]
+        public Guid SiteId { get; set; }
+    }
+}
---
+++ FBS.Service/SiteService.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FBS.Domain.Repository;
+using FBS.Service.ActionModels;
+using FBS.Domain.Aggregate.Entity;
+using FBS.Domain.Specifications;
+
+namespace FBS.Service
+{
+    public class SiteService
+    {
+        public SiteService()
+        {
+            this._rep = Factory.Factory<ISiteRepository>.GetConcrete();
+        }
+
+        /// <summary>
+        /// 设置网站信息
+        /// </summary>
+        /// <param name="cnf">设置网站信息</param>
+        public void SetSiteInfo(SiteCnf cnf)
+        {
+            this._site = this._rep.Find(new Specification<Site>(s => s.Id != Guid.Empty));
+            if (this._site == null)
+            {
+                this._site = new Site(cnf.SiteName, cnf.SiteDesc, cnf.SiteUrl, cnf.CopyRight, cnf.Version.ToString());
+                this._rep.Add(this._site);
+            }
+            else
+            {
+                this._site.SiteName = cnf.SiteName;
+                this._site.SiteDescription = cnf.SiteDesc;
+                this._site.Siteurl = cnf.SiteUrl;
+                this._site.Copyright = cnf.CopyRight;
+
+                this._rep.Update(this._site);
+            }
+            this._rep.PersistAll();
+        }
+
+        /// <summary>
+        /// 获取网站基本信息
+        /// </summary>
+        /// <returns></returns>
+        public SiteCnf GetSiteInfo()
+        {
+            SiteCnf info = new SiteCnf();
+
+            this._site = this._rep.Find(new Specification<Site>(s => s.Id != Guid.Empty));
+            if (this._site != null)
+            {
+                info.SiteName = this._site.SiteName;
+                info.SiteDesc = this._site.SiteDescription;
+                info.SiteUrl = this._site.Siteurl;
+                info.CopyRight = this._site.Copyright;
+                info.Version = new Version(this._site.Version);
+            }
+
+            return info;
+
+        }
+        public SitePage PageByName(string pageName) 
+        {
+            loadSite();
+            return this._rep.PageByName(this._site, pageName);
+        }
+        private void loadSite() 
+        {
+            if(this._site==null)
+                this._site=this._rep.Find(new Specification<Site>(s => s.Id != Guid.Empty));
+        }
+
+        public void ChangeTheme(string name)
+        {
+            loadSite();
+            if(this._site.Settings == null)
+                this._site.Settings = this._rep.SiteSettings(this._site);
+            if (this._site.Settings != null)
+                this._site.Settings.ThemeName = name;
+            this._rep.Update(this._site);
+            this._rep.PersistAll();
+        }
+        public string ThemeName()
+        {
+            loadSite();
+            return this._rep.SiteTheme(this._site);
+        }
+        private ISiteRepository _rep;
+        private Site _site;
+    }
+}
