41fced4f24659bcce872c4de269ba1896ce685e2
mike442144 <mike442144@localhost>
2013-01-17 01:42:32 +0800

    add tow references

---
+++ FBS.Domain/Repository/IBlogCategoryRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FBS.Domain.Aggregate.Entity;
+
+namespace FBS.Domain.Repository
+{
+    public interface IBlogCategoryRepository:IRepository<BlogStoryCategory>
+    {
+
+    }
+}
---
+++ FBS.Repository/BlogCategoryRepository.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FBS.Domain.Repository;
+using FBS.Domain.Aggregate.Entity;
+using FBS.Domain.Specifications;
+
+namespace FBS.Repository
+{
+    public class BlogCategoryRepository:IBlogCategoryRepository
+    {
+        private AviatorDb<BlogStoryCategory> _db = null;
+        public void Add(BlogStoryCategory entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Exists(ISpecification<BlogStoryCategory> specification)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Count(ISpecification<BlogStoryCategory> specification)
+        {
+            throw new NotImplementedException();
+        }
+
+        public BlogStoryCategory Find(ISpecification<BlogStoryCategory> specification)
+        {
+            throw new NotImplementedException();
+        }
+
+        public BlogStoryCategory GetByKey(Guid id)
+        {
+            BlogStoryCategory target = null;
+            var results = this._db.TEntitys.Where(bc => bc.Id == id);
+            var x = results.ToList();
+            if (x.Count > 0) 
+            {
+                target = x[0];
+            }
+            return target;
+        }
+
+        public IList<BlogStoryCategory> FindAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<BlogStoryCategory> FindAll(ISpecification<BlogStoryCategory> specification)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(BlogStoryCategory entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveByKey(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(BlogStoryCategory entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PersistAll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
